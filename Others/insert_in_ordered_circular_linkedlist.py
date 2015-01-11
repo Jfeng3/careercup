637da1990349675d193f41e990da137beabd84ce
jfeng1115 <jfeng1115@gmail.com>
2015-01-11 16:12:20 -0500

    added

---
+++ Others/insert_in_ordered_circular_linkedlist.py
@@ -0,0 +1,9 @@
+'''
+link: http://www.careercup.com/question?id=13273690
+user: jfeng
+company: Walmart
+type: Linkedlist
+desc: Insert an element in a ordered (ascending) circular linked list. After inserting return the node with the smallest element.
+'''
+ 
+    
--- others/insert_in_ordered_circular_linkedlist.py
+++
@@ -1,4 +0,0 @@
-'''
-http://www.careercup.com/question?id=13273690
-Insert an element in a ordered (ascending) circular linked list. After inserting return the node with the smallest element.
-'''
